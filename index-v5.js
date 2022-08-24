9739b5fd598276fe561029d35a7fe63f1c550da2
Rajib10098 <82735278+Rajib10098@users.noreply.github.com>
2022-08-24 19:27:40 +0530

    Update index-v5.js

--- index-v5.js
+++ index-v5.js
@@ -23,6 +23,9 @@ var descriptionContainer = document.getElementById('music-description-container'
 var musicDownlaodBtn = document.getElementById('music-download-btn');
 var downloadSongLink = document.getElementById('download-song-link');
 
+
+let smWiewAllBtnA = document.getElementById("sm-wiewAll-btn-a");
+
 var animationUndoX = NaN;
 var animationUndoY = 1;
 
@@ -32,11 +35,32 @@ var rotationRight = 0;
 var open = true;
 
 
+
+
+//Save user data start
+
+
+localStorage.setItem("GetLink", `${music.src}`)
+localStorage.setItem("title", `${document.title}`)
+localStorage.setItem("Surl", `${document.URL}`)
+localStorage.setItem("confrom", "true")
+
+//Save user data end
+
+// Music playPause btn Start
+if(localStorage.isMusicPlayed == "true"){
+    music.play();
+}
+
+
+
 playPause.addEventListener("click", function () {
     if (music.paused == true) {
+        localStorage.setItem("isMusicPlayed", true)
         music.play();
 
     } else {
+        localStorage.setItem("isMusicPlayed", false)
         music.pause();
     }
 })
@@ -59,20 +83,41 @@ music.addEventListener("ended", function () {
     pauseIcon.style.width = "0";
 })
 
+// Music playPause btn end
+
+//Repeat start
+
+if(localStorage.repeat == "true"){
+    localStorage.setItem('repeat', true)
+    music.loop = true;
+    repeatOneIcon.style.height = "40px"
+    repeatOneIcon.style.width = "40px"
+    repeatIcon.style.transform = "rotate(180deg)"
+}
+
+
+
 repeatSong.addEventListener("click", function () {
     if (music.loop == false) {
+        localStorage.setItem('repeat', true)
         music.loop = true;
         repeatOneIcon.style.height = "40px"
         repeatOneIcon.style.width = "40px"
         repeatIcon.style.transform = "rotate(180deg)"
     } else {
+        localStorage.setItem('repeat', false)
         music.loop = false;
         repeatIcon.style.transform = "rotate(-0deg)"
         repeatOneIcon.style.height = "40px"
         repeatOneIcon.style.width = "0"
-        
+
     }
 })
+
+
+//Repeat end
+
+
 
 leftSkipBtn.addEventListener("click", function () {
 
@@ -98,35 +143,55 @@ rightSkipBtn.addEventListener("click", function () {
     }
 
 })
+// Open close Description start
+
+if(localStorage.isscdDescrip == "true"){
+
+    localStorage.setItem("isscdDescrip", "true")
+    descriptionInfo.style.height = "0px";
+    descriptionInfo.style.paddingBottom = "0px"
+    descriptionInfo.style.paddingTop = "0px"
+    descriptionInfo.style.opacity = "0";
+    openCloseDescription.innerHTML = "View Description"
+    openCloseDescription.style.borderBottom = "none";
+    descriptionContainer.style.borderRadius = "5px"
+    open = false;
+
+}
 openCloseDescription.addEventListener("click", function () {
     if (open == true) {
+        localStorage.setItem("isscdDescrip", "true")
         descriptionInfo.style.height = "0px";
         descriptionInfo.style.paddingBottom = "0px"
         descriptionInfo.style.paddingTop = "0px"
         descriptionInfo.style.opacity = "0";
         openCloseDescription.innerHTML = "View Description"
-        openCloseDescription.style.borderBottom ="none";
-        descriptionContainer.style.borderRadius ="5px"
+        openCloseDescription.style.borderBottom = "none";
+        descriptionContainer.style.borderRadius = "5px"
         open = false;
     } else {
+        localStorage.setItem("isscdDescrip", "false")
         descriptionInfo.style.height = "100%";
         descriptionInfo.style.paddingBottom = "20px"
         descriptionInfo.style.paddingTop = "10px"
         descriptionInfo.style.opacity = "100";
         openCloseDescription.innerHTML = "Close Description";
-        openCloseDescription.style.borderBottom ="1px solid rgb(87, 87, 87)";
-        descriptionContainer.style.borderRadius ="16px"
+        openCloseDescription.style.borderBottom = "1px solid rgb(87, 87, 87)";
+        descriptionContainer.style.borderRadius = "16px"
         open = true;
     }
 })
 
-musicDownlaodBtn.addEventListener("click",function(){
-    musicDownlaodBtn.style.animation ="download-btn 0.5s";
-    musicDownlaodBtn.style.animationIterationCount ="5";
+
+// Open close Description End
+
+
+musicDownlaodBtn.addEventListener("click", function () {
+    musicDownlaodBtn.style.animation = "download-btn 0.5s";
+    musicDownlaodBtn.style.animationIterationCount = "5";
     animationUndoX = 0;
-   downloadSongLink.click(); // new line added
-    //location.href = music.src;
-   
+    window.location.href ='https://www.google.com/'
+
 })
 
 
@@ -136,13 +201,15 @@ musicDownlaodBtn.addEventListener("click",function(){
 
 //  ***********  Timer JS  **********
 function upadateTimer() {
+// console.log(music.currentTime);
 
-      animationUndoX = animationUndoX + animationUndoY;
-      if (animationUndoX > 14) {
+
+    animationUndoX = animationUndoX + animationUndoY;
+    if (animationUndoX > 14) {
         animationUndoX = NaN;
-        musicDownlaodBtn.style.animation ="";
+        musicDownlaodBtn.style.animation = "";
     }
-   
+
 
     seekSlider.style.width = music.currentTime * 100 / music.duration + "%"
     if (music.ended == true) {
@@ -164,10 +231,58 @@ function upadateTimer() {
     tottalDuration.textContent = durationMin + ":" + durationSec;
 }
 
+
+
 setInterval(upadateTimer, 200)
 
 
 
-// Save the down info 
+     
+        window.addEventListener("load", function () {
+            
 
-// Save the down info
+            smWiewAllBtnA.href = `https://cinkmusic.blogspot.com/search?q=${encodeURIComponent(smLabel)}`
+
+
+            fetch(`https://www.googleapis.com/blogger/v3/blogs/6002919485693520244/posts/search?q=${encodeURIComponent(smLabel)}&key=AIzaSyDM7PgWDnsmjmusZ1K7sfoeBQCcOSHkLmA`).then((response) => response.json()).then((data) => {
+
+                let displaySeeMore = "";
+                for (let i = 0; i < data.items.length; i++) {
+                    let findImage = data.items[i].content
+                    let resizingTheImage = new URL(findImage.match(/\bhttps?:\/\/blogger.googleusercontent.com\/\S+/gi)).href.replace("s1600", "s150")
+
+
+
+                    displaySeeMore += `
+
+      <div class="SeeMoreContainer">
+        
+        <a style="text-decoration: none;" href="${data.items[i].url}">
+            <div class="SeeMoreImageContainer"><p class="smLoading">Loading...</p>
+            <img class="SeeMoreImage" src="${resizingTheImage}" alt="" loading="lazy">
+        </div>
+        </a>
+
+        <div class="SeeMoretitleContainer">
+            <a style="text-decoration: none;" href="${data.items[i].url}"><p class="SeeMoreSongTitle">${data.items[i].title}</p></a>
+        </div>
+
+      </div>
+      `
+
+
+
+
+
+                    document.getElementById("See-more-container").innerHTML = displaySeeMore;
+
+                }
+
+
+
+
+            })
+
+
+        })
+
